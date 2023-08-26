8ca00aee917c37358ca3318d2a206fbecb26d3a9
Tagellajore <tagelsft9@gmail.com>
2023-08-26 15:15:36 -0400

    removing logs

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -4,7 +4,6 @@ import "../styles/TopicListItem.scss";
 
 // component for displaying the topic in top navigation bar
 const TopicListItem = (props) => {
-  console.log("props from topiclistitem", props);
   return (
     <div className="topic-list__item">
       <span onClick={() => props.topicClicked(props.topicId)}>
